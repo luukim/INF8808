1f9bac5923694beb78162fecb5cf57101624fdfb
Souhaila Mellouk <souhaila.mellouk@polymtl.ca>
2021-01-26 17:22:06 +0100

    début tp2 : preprocess

---
+++ TP2/src/hover_template.py
@@ -0,0 +1,33 @@
+'''
+    Provides the template for the hover tooltips.
+'''
+from modes import MODES
+
+
+def get_hover_template(name, mode):
+    '''
+        Sets the template for the hover tooltips.
+
+        The template contains:
+            * A title stating the hovered element's x value, with:
+                - Font family: Grenze Gotish
+                - Font size: 24px
+                - Font color: Black
+            * A bold label for the player name followed
+                by the hovered elements's player's name
+            * A bold label for the player's lines
+                followed by:
+                - The number of lines if the mode is 'Count'
+                - The percent of lines fomatted with two
+                    decimal points followed by a '%' symbol
+                    if the mode is 'Percent'.
+
+        Args:
+            name: The hovered element's player's name
+            mode: The current display mode
+        Returns:
+            The hover template with the elements descibed above
+    '''
+    # TODO: Generate and return the over template
+
+    return ''
