16363e0050795be78bdaaf9a4033797ebeb9ce9f
Souhaila Mellouk <souhaila.mellouk@polymtl.ca>
2021-03-14 00:44:53 +0100

    add hover

--- TP4/code/src/bubble.py
+++ TP4/code/src/bubble.py
@@ -25,7 +25,10 @@ def get_plot(my_df, gdp_range, co2_range):
             The generated figure
     '''
     # TODO : Define figure with animation
-    return None
+    fig = px.scatter(x=my_df['GDP'], y=my_df['CO2'], color=my_df["Continent"], log_x=True, log_y=True, size=my_df['Population'], animation_frame=my_df['Year'],
+                     animation_group=my_df['Country Name'], hover_name=my_df['Country Name'], size_max=30, range_x=gdp_range, range_y=co2_range)
+    fig.update_traces(marker=dict(sizemin=5))
+    return fig
 
 
 def update_animation_hover_template(fig):
@@ -41,7 +44,8 @@ def update_animation_hover_template(fig):
     '''
 
     # TODO : Set the hover template
-    return None
+    fig.update_traces(hovertemplate=hover_template.get_bubble_hover_template())
+    return fig
 
 
 def update_animation_menu(fig):
@@ -55,7 +59,10 @@ def update_animation_menu(fig):
             The updated figure
     '''
     # TODO : Update animation menu
-    return None
+    fig.update_layout(sliders=[dict(currentvalue=dict(prefix='Data for year : '))],
+                    updatemenus=[dict(type="buttons",buttons=[dict(label="Animate", method="animate"), dict(visible=False)])])
+                                        
+    return fig
 
 
 def update_axes_labels(fig):
@@ -68,7 +75,11 @@ def update_axes_labels(fig):
             The updated figure
     '''
     # TODO : Update labels
-    return None
+    fig.update_layout(
+        xaxis=dict(title='GDP per capita ($ USD)'),
+        yaxis=dict(title='CO2 emissions per capita (metric tonnes)'),
+    )
+    return fig
 
 
 def update_template(fig):
@@ -82,7 +93,8 @@ def update_template(fig):
             The updated figure
     '''
     # TODO : Update template
-    return None
+    fig.update_layout(template='simple_white')
+    return fig
 
 
 def update_legend(fig):
@@ -95,4 +107,5 @@ def update_legend(fig):
             The updated figure
     '''
     # TODO : Update legend
-    return None
+    fig.update_layout(legend_title="Legend")
+    return fig
--- TP4/code/src/hover_template.py
+++ TP4/code/src/hover_template.py
@@ -17,5 +17,9 @@ def get_bubble_hover_template():
             The content of the tooltip
     '''
     # TODO : Generate tooltip
+    style_country = "<b style = 'font-weight:bold'>Country : </b>" + "<b style = 'font-weight:normal'>%{country}</b>" 
+    style_population = "<br><b style = 'font-weight:bold'>Population : </b >" + "<b style = 'font-weight:normal'>{size} </b>"
+    style_gdp = "<br><b style = 'font-weight:bold'>GDP : </b>" + "<b style = 'font-weight:normal'>%{x} $ (USD)</b>"
+    style_co2 = "<br><b style = 'font-weight:bold'>CO2 : </b>" + "<b style = 'font-weight:normal'> %{y} metric tonnes </b>"
 
-    return ''
+    return style_country + style_population + style_gdp + style_co2 +  "<extra></extra>"
